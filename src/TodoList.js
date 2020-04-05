531943749425c06c5cc820f9807e7f83eea342c0
saori <srjm570@gmail.com>
2020-04-05 20:53:09 +0900

    完了と未完了の切り替え

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,6 +7,7 @@ class TodoList extends Component {
             <Todo
                 key={todo.id}
                 {...todo}
+                setStatus={this.props.setTodoStatus}
             />
         )
 
