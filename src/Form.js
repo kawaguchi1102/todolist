15144b86b0225cebe7b6ce9037ee7707923d9b3a
saori <srjm570@gmail.com>
2020-04-05 23:53:54 +0900

    フォーム入力時にボタンをアクティブにする処理を追加 ＊途中まで

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,25 @@ import React, { Component} from "react";
 import './css/form.css';
 
 class Form extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state={
+            title:''
+        }
+    }
+
+    handleTitleChange(place) {
+        console.log(place);
+        this.setState({place});
+        //Todo:「やること」が入力されたら、ボタンをアクティブにする
+    }
+
     render() {
         return (
             <div className="form">
                 <form onSubmit={this.props.handleSubmit}>
-                    <input className="form-title" name="title" type="text" placeholder="やること"/>
+                    <input className="form-title" name="title" type="text" value={this.state.title} onChange={e => this.handleTitleChange(e.target.value)} placeholder="やること"/>
                     <textarea className="form-desc" name="desc" placeholder="説明を入力" cols="30" rows="2"></textarea>
                     <button className="form-button" type="submit">Todoを作成</button>
                 </form>
