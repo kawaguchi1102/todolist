65f091e3bc82669fc9836fb5e9a007530751f50e
saori <srjm570@gmail.com>
2020-04-05 17:44:28 +0900

    Formを追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import TodoList from './TodoList';
+import Form from './Form';
 import './css/App.css';
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      todos: [
+    const todos = [
         {
           id: 1,
           title: "Hello, React!",
@@ -20,18 +20,47 @@ class App extends Component {
           done: true
         }
       ]
+      this.state = {
+        todos: todos,
+        countTodo: todos.length + 1,
+      }
     }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      const title = e.target.title.value;
+      const desc = e.target.desc.value;
+      const todos = this.state.todos.slice()
+      const countTodo = this.state.countTodo
+
+      todos.push({
+        id: countTodo,
+        title: title,
+        desc: desc,
+        done: false,
+      });
+
+      this.setState({ todos })
+      this.setState({ countTodo: countTodo + 1 })
+
+      e.target.title.value = '';
+      e.target.desc.value = '';
   }
 
   render() {
     return (
         <div className="main-area">
+
           <div className="content-area">
             <h1 className="page-title">ReactでTodoアプリをつくってみた</h1>
+
+            <Form handleSubmit={this.handleSubmit.bind(this)} />
+
             <TodoList
               todos={this.state.todos}
             />
           </div>
+
         </div>
     );
   }
