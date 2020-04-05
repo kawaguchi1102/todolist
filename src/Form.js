aea708df8619af964ca4dccde30903460c02cecc
kawaguchi <info@kawaguchi-design.com>
2020-04-06 01:09:14 +0900

    「やること」タイトル入力時にactive class付与

--- src/Form.js
+++ src/Form.js
@@ -10,19 +10,26 @@ class Form extends Component {
         }
     }
 
-    handleTitleChange(place) {
-        console.log(place);
-        this.setState({place});
-        //Todo:「やること」が入力されたら、ボタンをアクティブにする
+    handleTitleChange(title) {
+        this.setState({title});
     }
 
     render() {
         return (
             <div className="form">
-                <form onSubmit={this.props.handleSubmit}>
-                    <input className="form-title" name="title" type="text" value={this.state.title} onChange={e => this.handleTitleChange(e.target.value)} placeholder="やること"/>
-                    <textarea className="form-desc" name="desc" placeholder="説明を入力" cols="30" rows="2"></textarea>
-                    <button className="form-button" type="submit">Todoを作成</button>
+                <form onSubmit = {this.props.handleSubmit}>
+                    <input
+                      className = "form-title"
+                      name = "title"
+                      type = "text"
+                      onChange = { e => this.handleTitleChange(e.target.value) }
+                      placeholder = "やること"
+                    />
+                    <textarea className="form-desc" name="desc" placeholder="説明を入力" cols="30" rows="2" />
+                    <button
+                      className={this.state.title === '' ? 'form-button' : 'form-button form-button--active'}
+                      type="submit"
+                    >Todoを作成</button>
                 </form>
             </div>
         );
